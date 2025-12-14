dc1122d4bee0d97c9771d808b53ea3d16216187b
vickyvaf <vickyadi243@gmail.com>
2025-12-15 00:08:16 +0700

    feat: add footer in main screen

--- src/components/canvas/Avatar.tsx
+++ src/components/canvas/Avatar.tsx
@@ -1,6 +1,5 @@
 import { useAnimations, useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { faceData } from "../../utils/faceStore";
 import React, {
   forwardRef,
   useEffect,
@@ -8,6 +7,7 @@ import React, {
   useRef,
 } from "react";
 import { Group } from "three";
+import { faceData } from "../../utils/faceStore";
 
 useGLTF.preload("/face-mocap.glb");
 
@@ -17,13 +17,10 @@ export const Avatar = forwardRef<Group, React.JSX.IntrinsicElements["group"]>(
     const { nodes, animations } = useGLTF("/face-mocap.glb");
     const { actions, names } = useAnimations(animations, group);
 
-    // Expose the group ref to parent
     useImperativeHandle(ref, () => group.current as Group);
 
-    // Play all animations by default or log them
     useEffect(() => {
       if (names.length > 0) {
-        // Play the first animation found
         actions[names[0]]?.reset().fadeIn(0.5).play();
         actions[names[1]]?.reset().fadeIn(0.5).play();
       }
@@ -33,33 +30,19 @@ export const Avatar = forwardRef<Group, React.JSX.IntrinsicElements["group"]>(
       };
     }, [actions, names]);
 
-    // Apply Real-time Face Data
     useFrame(() => {
-      const { eyeLeft, eyeRight } = faceData;
+      const { head } = faceData;
 
-      // Apply rotation to eye bones/meshes
-      // Note: Check axis orientation relative to model.
-      // Gaze X (Left/Right) usually maps to Y rotation (Yaw)
-      // Gaze Y (Up/Down) usually maps to X rotation (Pitch)
-
-      if (nodes.eye_left) {
-        nodes.eye_left.rotation.x = eyeLeft.y;
-        nodes.eye_left.rotation.y = eyeLeft.x;
-      }
-
-      if (nodes.eye_right) {
-        nodes.eye_right.rotation.x = eyeRight.y;
-        nodes.eye_right.rotation.y = eyeRight.x;
+      if (nodes.body) {
+        nodes.body.rotation.set(head.x, head.y, head.z);
+        // nodes.eye.rotation.set(head.x, head.y, head.z);
       }
     });
 
     return (
       <group ref={group} {...props} dispose={null}>
         <primitive object={nodes.body} />
-        <group rotation={[0, Math.PI, 0]}>
-          <primitive object={nodes.eye_left} />
-          <primitive object={nodes.eye_right} />
-        </group>
+        {/* <primitive object={nodes.eye} /> */}
         <primitive object={nodes.lips} />
       </group>
     );
